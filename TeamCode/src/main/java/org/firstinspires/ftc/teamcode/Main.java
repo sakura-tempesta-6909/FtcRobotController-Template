cbf754bc39a6b1ef12cd48230d598c48d5c5a74a
Yuki Terasaki <tenko0316yuki1@gmail.com>
2024-12-02 15:43:38 +0900

    Create Main.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,77 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Main OpMode", group = "Main")
+public class Main extends OpMode {
+
+    /*
+     * This is executed once after the driver presses INIT.
+     * ドライバーがINITを押した後、1度実行される
+     */
+    @Override
+    public void init() {
+    }
+
+    /*
+     * This is executed repeatedly between INIT and PLAY.
+     * ドライバーがINITを押した後からPLAYを押すまでの間、繰り返し実行される
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * This is executed once at the start.
+     * 開始時に一度だけ実行される
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * This runs continuously while enabled.
+     * Enableの間ずっと実行される
+     */
+    @Override
+    public void loop() {
+
+    }
+
+    /*
+     * This is executed once when the code is stopped.
+     * コードが停止されるときに一度だけ実行される
+     */
+    @Override
+    public void stop() {
+    }
+}
